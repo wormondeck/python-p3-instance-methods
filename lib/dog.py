d0378e03487ff65c34bfbe3ec03c9fc2947efea1
Ronney Arias <ronneyarias@gmail.com>
2024-05-09 11:27:10 -0500

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,11 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
+
+fido = Dog()
+fido.bark()
+
